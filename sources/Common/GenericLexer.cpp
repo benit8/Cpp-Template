85674a91463b0ddbaab5751a39f650c17529fdfe
Benoit Lormeau <blormeau@outlook.com>
2021-07-03 15:45:00 +0200

    Update the GenericLexer

--- sources/Common/GenericLexer.cpp
+++ sources/Common/GenericLexer.cpp
@@ -5,78 +5,12 @@
 */
 
 #include "Common/GenericLexer.hpp"
-#include <algorithm>
-#include <cassert>
 
 ////////////////////////////////////////////////////////////////////////////////
 
-GenericLexer::GenericLexer(std::string_view input)
-: m_input(input)
-{}
-
-////////////////////////////////////////////////////////////////////////////////
-
-// Tells whether the parser's index has reached input's end
-bool GenericLexer::at_end() const
-{
-	return m_index >= m_input.length();
-}
-
-// Returns the current character at the parser index, plus `offset` if specified
-char GenericLexer::peek(size_t offset) const
-{
-	return (m_index + offset < m_input.length()) ? m_input[m_index + offset] : '\0';
-}
-
-// Tests the next character in the input
-bool GenericLexer::next_is(char expected) const
-{
-	return peek() == expected;
-}
-
-// Tests if the `expected` string comes next in the input
-bool GenericLexer::next_is(std::string_view expected) const
-{
-	for (size_t i = 0; i < expected.length(); ++i)
-		if (peek(i) != expected[i])
-			return false;
-	return true;
-}
-
-// Tests the next character against a Condition
-bool GenericLexer::next_is(Condition condition) const
-{
-	return condition(peek());
-}
-
-// Consume a character and advance the parser index
-char GenericLexer::consume()
-{
-	assert(!at_end());
-	return m_input[m_index++];
-}
-
-// Consume the given character if it is next in the input
-bool GenericLexer::consume_specific(char specific)
-{
-	if (peek() != specific)
-		return false;
-
-	ignore();
-	return true;
-}
-
-// Consume the given string if it is next in the input
-bool GenericLexer::consume_specific(std::string_view str)
-{
-	if (!next_is(str))
-		return false;
-
-	ignore(str.length());
-	return true;
-}
-
-// Consume a number of characters
+/**
+ * Consume a number of characters.
+ */
 std::string_view GenericLexer::consume(size_t count)
 {
 	if (count == 0)
@@ -89,10 +23,12 @@ std::string_view GenericLexer::consume(size_t count)
 	return m_input.substr(start, length);
 }
 
-// Consume the rest of the input
+/**
+ * Consume the rest of the input.
+ */
 std::string_view GenericLexer::consume_all()
 {
-	if (at_end())
+	if (is_eof())
 		return {};
 
 	auto rest = m_input.substr(m_index, m_input.length() - m_index);
@@ -100,11 +36,13 @@ std::string_view GenericLexer::consume_all()
 	return rest;
 }
 
-// Consume until a new line is found
+/**
+ * Consume until a new line is found.
+ */
 std::string_view GenericLexer::consume_line()
 {
 	size_t start = m_index;
-	while (!at_end() && peek() != '\r' && peek() != '\n')
+	while (!is_eof() && peek() != '\r' && peek() != '\n')
 		m_index++;
 	size_t length = m_index - start;
 
@@ -116,25 +54,14 @@ std::string_view GenericLexer::consume_line()
 	return m_input.substr(start, length);
 }
 
-// Consume and return characters while `condition` returns true
-std::string_view GenericLexer::consume_while(Condition condition)
-{
-	size_t start = m_index;
-	while (!at_end() && condition(peek()))
-		m_index++;
-	size_t length = m_index - start;
-
-	if (length == 0)
-		return {};
-	return m_input.substr(start, length);
-}
-
-// Consume and return characters until `stop` is peek'd
-// The `stop` character is ignored, as it is user-defined
+/**
+ * Consume and return characters until `stop` is peek'd.
+ * The `stop` character is ignored, as it is user-defined.
+ */
 std::string_view GenericLexer::consume_until(char stop)
 {
 	size_t start = m_index;
-	while (!at_end() && peek() != stop)
+	while (!is_eof() && peek() != stop)
 		m_index++;
 	size_t length = m_index - start;
 
@@ -145,94 +72,66 @@ std::string_view GenericLexer::consume_until(char stop)
 	return m_input.substr(start, length);
 }
 
-// Consume and return characters until the string `stop` is found
-// The `stop` string is ignored, as it is user-defined
-std::string_view GenericLexer::consume_until(std::string_view stop)
+/**
+ * Consume and return characters until the string `stop` is found.
+ * The `stop` string is ignored, as it is user-defined.
+ */
+std::string_view GenericLexer::consume_until(const char* stop)
 {
 	size_t start = m_index;
-	while (!at_end() && !next_is(stop))
+	while (!is_eof() && !next_is(stop))
 		m_index++;
 	size_t length = m_index - start;
 
-	ignore(stop.length());
+	ignore(__builtin_strlen(stop));
 
 	if (length == 0)
 		return {};
 	return m_input.substr(start, length);
 }
 
-// Consume and return characters until `condition` return true
-std::string_view GenericLexer::consume_until(Condition condition)
+/**
+ * Consume a string surrounded by single or double quotes. The returned
+ * std::string_view does not include the quotes. An escape character can be provided
+ * to capture the enclosing quotes. Please note that the escape character will
+ * still be in the resulting std::string_view
+ */
+std::string_view GenericLexer::consume_quoted_string(char escape_char)
 {
-	size_t start = m_index;
-	while (!at_end() && !condition(peek()))
-		m_index++;
-	size_t length = m_index - start;
-
-	if (length == 0)
-		return {};
-	return m_input.substr(start, length);
-}
-
-// Consume a string surrounded by single or double quotes
-// The returned string view does not include the quotes
-std::optional<std::string_view> GenericLexer::consume_quoted_string()
-{
-	if (!next_is('\'') && !next_is('"'))
+	if (!next_is(is_quote))
 		return {};
 
-	char quoteChar = consume();
+	char quote_char = consume();
 	size_t start = m_index;
-	while (!at_end() && !next_is(quoteChar))
+	while (!is_eof()) {
+		if (next_is(escape_char))
+			m_index++;
+		else if (next_is(quote_char))
+			break;
 		m_index++;
+	}
 	size_t length = m_index - start;
 
-	if (peek() != quoteChar) {
-		m_index = start - 1; // Restore the index in case the string is unterminated
+	if (peek() != quote_char) {
+		// Restore the index in case the string is unterminated
+		m_index = start - 1;
 		return {};
 	}
 
+	// Ignore closing quote
 	ignore();
 
 	return m_input.substr(start, length);
 }
 
-// Ignore a number of characters (1 by default)
-void GenericLexer::ignore(size_t count)
+std::string GenericLexer::consume_and_unescape_string(char escape_char)
 {
-	count = std::min(count, m_input.length() - m_index);
-	m_index += count;
-}
+	auto view = consume_quoted_string(escape_char);
+	if (view.empty())
+		return {};
 
-// Ignore characters while `condition` returns true
-void GenericLexer::ignore_while(Condition condition)
-{
-	while (!at_end() && condition(peek()))
-		m_index++;
-}
-
-// Ignore characters until `stop` is peek'd
-// The `stop` character is ignored as it is user-defined
-void GenericLexer::ignore_until(char stop)
-{
-	while (!at_end() && !next_is(stop))
-		m_index++;
-	ignore();
-}
-
-// Ignore characters until the string `stop` is found
-// The `stop` string is ignored, as it is user-defined
-void GenericLexer::ignore_until(std::string_view stop)
-{
-	while (!at_end() && !next_is(stop))
-		m_index++;
-	ignore(stop.length());
-}
-
-// Ignore characters until `condition` return true
-// We don't skip the stop character as it may not be a single value
-void GenericLexer::ignore_until(Condition condition)
-{
-	while (!at_end() && !condition(peek()))
-		m_index++;
+	std::string str;
+	for (size_t i = 0; i < view.length(); ++i)
+		str.push_back(consume_escaped_character(escape_char));
+	return str;
 }
--- sources/Common/GenericLexer.hpp
+++ sources/Common/GenericLexer.hpp
@@ -8,6 +8,8 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
+#include "Assertions.hpp"
+#include <algorithm>
 #include <functional>
 #include <optional>
 #include <string_view>
@@ -17,41 +19,177 @@
 class GenericLexer
 {
 public:
-	explicit GenericLexer(std::string_view input);
-	virtual ~GenericLexer() = default;
+	constexpr explicit GenericLexer(const std::string_view& input)
+	: m_input(input)
+	{}
 
-	using Condition = std::function<bool(char)>;
+	constexpr size_t tell() const { return m_index; }
+	constexpr size_t tell_remaining() const { return m_input.length() - m_index; }
 
-	size_t tell() const { return m_index; }
-	size_t tell_remaining() const { return m_input.length() - m_index; }
+	std::string_view remaining() const { return m_input.substr(m_index); }
 
-	bool at_end() const;
+	constexpr bool is_eof() const { return m_index >= m_input.length(); }
 
-	char peek(size_t offset = 0) const;
+	constexpr char peek(size_t offset = 0) const
+	{
+		return (m_index + offset < m_input.length()) ? m_input[m_index + offset] : '\0';
+	}
 
-	bool next_is(char expected) const;
-	bool next_is(std::string_view expected) const;
-	bool next_is(Condition) const;
+	constexpr bool next_is(char expected) const
+	{
+		return peek() == expected;
+	}
 
-	char consume();
-	bool consume_specific(char specific);
-	bool consume_specific(std::string_view str);
+	constexpr bool next_is(std::string_view expected) const
+	{
+		for (size_t i = 0; i < expected.length(); ++i)
+			if (peek(i) != expected[i])
+				return false;
+		return true;
+	}
+
+	constexpr bool next_is(const char* expected) const
+	{
+		for (size_t i = 0; expected[i] != '\0'; ++i)
+			if (peek(i) != expected[i])
+				return false;
+		return true;
+	}
+
+	constexpr void retreat()
+	{
+		ASSERT(m_index > 0);
+		--m_index;
+	}
+
+	constexpr char consume()
+	{
+		ASSERT(!is_eof());
+		return m_input[m_index++];
+	}
+
+	template<typename T>
+	constexpr bool consume_specific(const T& next)
+	{
+		if (!next_is(next))
+			return false;
+
+		if constexpr (requires { next.length(); })
+			ignore(next.length());
+		else
+			ignore(sizeof(next));
+		return true;
+	}
+
+	constexpr bool consume_specific(const char* next)
+	{
+		return consume_specific(std::string_view { next });
+	}
+
+	constexpr char consume_escaped_character(char escape_char = '\\', const std::string_view& escape_map = "n\nr\rt\tb\bf\f")
+	{
+		if (!consume_specific(escape_char))
+			return consume();
+
+		auto c = consume();
+		for (size_t i = 0; i < escape_map.length(); i += 2) {
+			if (c == escape_map[i])
+				return escape_map[i + 1];
+		}
+		return c;
+	}
+
 	std::string_view consume(size_t count);
 	std::string_view consume_all();
 	std::string_view consume_line();
-	std::string_view consume_while(Condition);
-	std::string_view consume_until(char stop);
-	std::string_view consume_until(std::string_view stop);
-	std::string_view consume_until(Condition);
-	std::optional<std::string_view> consume_quoted_string();
+	std::string_view consume_until(char);
+	std::string_view consume_until(const char*);
+	std::string_view consume_quoted_string(char escape_char = 0);
+	std::string consume_and_unescape_string(char escape_char = '\\');
 
-	void ignore(size_t count = 1);
-	void ignore_while(Condition);
-	void ignore_until(char stop);
-	void ignore_until(std::string_view stop);
-	void ignore_until(Condition);
+	constexpr void ignore(size_t count = 1)
+	{
+		count = std::min(count, m_input.length() - m_index);
+		m_index += count;
+	}
+
+	constexpr void ignore_until(char stop)
+	{
+		while (!is_eof() && peek() != stop) {
+			++m_index;
+		}
+		ignore();
+	}
+
+	constexpr void ignore_until(const char* stop)
+	{
+		while (!is_eof() && !next_is(stop)) {
+			++m_index;
+		}
+		ignore(__builtin_strlen(stop));
+	}
+
+	// Test the next character against a Condition
+	template<typename TPredicate>
+	constexpr bool next_is(TPredicate pred) const
+	{
+		return pred(peek());
+	}
+
+	// Consume and return characters while `pred` returns true
+	template<typename TPredicate>
+	std::string_view consume_while(TPredicate pred)
+	{
+		size_t start = m_index;
+		while (!is_eof() && pred(peek()))
+			++m_index;
+		size_t length = m_index - start;
+
+		if (length == 0)
+			return {};
+		return m_input.substr(start, length);
+	}
+
+	// Consume and return characters until `pred` return true
+	template<typename TPredicate>
+	std::string_view consume_until(TPredicate pred)
+	{
+		size_t start = m_index;
+		while (!is_eof() && !pred(peek()))
+			++m_index;
+		size_t length = m_index - start;
+
+		if (length == 0)
+			return {};
+		return m_input.substr(start, length);
+	}
+
+	// Ignore characters while `pred` returns true
+	template<typename TPredicate>
+	constexpr void ignore_while(TPredicate pred)
+	{
+		while (!is_eof() && pred(peek()))
+			++m_index;
+	}
+
+	// Ignore characters until `pred` return true
+	// We don't skip the stop character as it may not be a unique value
+	template<typename TPredicate>
+	constexpr void ignore_until(TPredicate pred)
+	{
+		while (!is_eof() && !pred(peek()))
+			++m_index;
+	}
 
 protected:
 	std::string_view m_input;
 	size_t m_index { 0 };
 };
+
+constexpr auto is_any_of(const std::string_view& values)
+{
+	return [values](auto c) { return values.find(c) != std::string_view::npos; };
+}
+
+constexpr auto is_path_separator = is_any_of("/\\");
+constexpr auto is_quote = is_any_of("'\"");
